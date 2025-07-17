4bfcd6b85124275ac3bfb0a7b7de34230dc693b2
Alexander Chibizov <120022847+evosome@users.noreply.github.com>
2025-07-17 22:45:17 +0400

    Added character attacks and camera effect

--- src/arenas/Arena.gd
+++ src/arenas/Arena.gd
@@ -6,15 +6,26 @@ var _character_map: Dictionary[PlayerInfo, PlayerCharacter] = {}
 @export var _entities_container: Node
 
 @export var _spawnpoints: Array[CharacterSpawnpoint]
+@export var _line_of_ground: Path2D
 
 
-func create_character_for(player: PlayerInfo, charater_type: CharacterType) -> void:
-	if _character_map.has(player):
-		push_error("Character for the player was already assigned")
-		return
-	var spawnpoint = get_free_spawnpoint()
-	var character = PlayerCharacter.spawn(self, spawnpoint, charater_type)
-	_character_map[player] = character
+#region getter/setter
+
+func get_camera() -> ArenaCamera:
+	return _camera
+
+
+func get_entities() -> Array[Node]:
+	return _entities_container.get_children()
+
+
+func get_free_spawnpoint_position() -> Vector2:
+	var node: Node2D = _spawnpoints.pop_front()
+	return node.position
+
+
+func get_free_spawnpoint() -> CharacterSpawnpoint:
+	return _spawnpoints.pop_front()
 
 
 func get_character_of(player: PlayerInfo) -> PlayerCharacter:
@@ -34,6 +45,24 @@ func get_characters_of(players: Array[PlayerInfo]) -> Array[PlayerCharacter]:
 	return chatacters
 
 
+## Line of ground represented as 2D curve
+func get_curve_of_ground() -> Curve2D:
+	return _line_of_ground.curve
+
+#endregion
+
+
+#region public
+
+func create_character_for(player: PlayerInfo, charater_type: CharacterType) -> void:
+	if _character_map.has(player):
+		push_error("Character for the player was already assigned")
+		return
+	var spawnpoint = get_free_spawnpoint()
+	var character = PlayerCharacter.spawn(self, spawnpoint, charater_type)
+	_character_map[player] = character
+
+
 func spawn(entity: Node) -> void:
 	_entities_container.add_child(entity)
 
@@ -47,23 +76,6 @@ func spawn_character_at(character: PlayerCharacter, spawnpoint: CharacterSpawnpo
 
 func despawn(entity: Node) -> void:
 	_entities_container.remove_child(entity)
-
-
-func get_camera() -> ArenaCamera:
-	return _camera
-
-
-func get_entities() -> Array[Node]:
-	return _entities_container.get_children()
-
-
-func get_free_spawnpoint_position() -> Vector2:
-	var node: Node2D = _spawnpoints.pop_front()
-	return node.position
-
-
-func get_free_spawnpoint() -> CharacterSpawnpoint:
-	return _spawnpoints.pop_front()
 
 
 ## This method is asynchronous.
@@ -80,3 +92,5 @@ func async_smash(smash_info: SmashInfo) -> void:
 ## Asynchronously show animations when nobody wins
 func async_draw() -> void:
 	pass
+
+#endregion
